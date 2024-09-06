4bcd730ef79086e33518c7c5ef0260a25757411d
Anastasia Shkundich <shkundich30@gmail.com>
2024-09-06 13:16:32 +0300

    lab 2

---
+++ lab 2/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+
+// Вариант 30
+using namespace std;
+int main() {
+	double x, y, z, result;
+	cout << "Input x: ";
+	cin >> x;
+	cout << "Input y: ";
+	cin >> y;
+	cout << "Input z: ";
+	cin >> z;
+	result = log(pow(y, -sqrt(abs(x)))) * (x - (y / 2)) * (1 + (pow(z, 2) / (3 - (pow(z, 2) / 5))));
+	cout << "Result is: " << result;
+	return 0;
+}

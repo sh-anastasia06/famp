cedcc88a0af87f1d4c243d959f78fd5693741e15
Anastasia Shkundich <shkundich30@gmail.com>
2024-09-07 10:43:31 +0300

    lab 3

---
+++ lab 3/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+// Вариант 15
+int main()
+{
+    double x, y, result;
+
+    cout <<  "Input x: ";
+    cin >> x;
+    cout << "Input y: ";
+    cin >> y;
+
+    if (y == 0) {
+        cout << "\nWrong input:\n\ty cannot be 0\n";
+        return 0;
+    }
+
+    if (x / y < 0) {
+        result = pow((pow(x, 2) + y), 3);
+    }
+    else if (x / y > 0) {
+        result = log(abs(x / y)) + (x / y);
+    }
+    else {
+        result = pow((abs(sin(y))), 1 / 3);
+    }
+
+    cout << "\nResult is = " << result << endl;
+    return 0;
+}
+
---
+++ lab 3/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int k;
+    cout << "Input k: ";
+    cin >> k;
+    if (k < 1 || k > 365) {
+        cout << "\nWrong input:\n\tk must be higher than 1 and lower than 365" << endl;
+        return 0;
+    }
+    k = k % 7;
+    switch (k)
+    {
+    case 1:
+        cout << "It's monday" << endl;
+        break;
+    case 2:
+        cout << "It's tuesday" << endl;
+        break;
+    case 3:
+        cout << "It's wednesday" << endl;
+        break;
+    case 4:
+        cout << "It's thursday" << endl;
+        break;
+    case 5:
+        cout << "It's friday" << endl;
+        break;
+    case 6:
+        cout << "It's saturday" << endl;
+        break;
+    default:
+        cout << "It's sunday" << endl;
+        break;
+    }
+    return 0;
+}
+

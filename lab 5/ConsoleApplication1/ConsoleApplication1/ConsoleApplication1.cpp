41dd8442793aeff6a6e8e6bff31f9b2fcdf3cb19
Anastasia Shkundich <shkundich30@gmail.com>
2024-09-08 12:59:20 +0300

    lab 5

---
+++ lab 5/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    srand(time(0));
+    int fromNum = 30, toNum = fromNum + 20, count = 1, userNumber;
+    int randomNumber = rand() % (toNum - fromNum + 1) + fromNum;
+    cout << "Try to guess number from " << fromNum << " to " << toNum << " : ";
+    cin >> userNumber;
+    while (userNumber != randomNumber) {
+        count++;
+        if (userNumber < randomNumber) {
+            cout << "\nMy number is higher";
+        }
+        else {
+            cout << "\nMy number is lower";
+        }
+        cout << "\nTry one more time: ";
+        cin >> userNumber;
+        if (userNumber == randomNumber) {
+            break;
+        }
+    }
+
+    cout << "\nYou won!!!";
+    cout << "\nNumber of attempts: " << count << endl;
+    return 0;
+}
---
+++ lab 5/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int userYear, userMonth, userDay, daysInMonth;
+    cout << "Your birth date info";
+    do {
+        cout << "\n\tInput year: ";
+        cin >> userYear;
+    } while (userYear <= 1924 || userYear > 2024);
+    do {
+        cout << "\tInput month: ";
+        cin >> userMonth;
+    } while (userMonth < 1 || userMonth > 12);
+    switch (userMonth)
+    {case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        daysInMonth = 31;
+        break;
+    case 2:
+        if (userYear % 4 == 0) {
+            daysInMonth = 29;
+        }
+        else {
+            daysInMonth = 28;
+        }
+        break;
+    default:
+        daysInMonth = 30;
+        break;
+    }
+    do {
+        cout << "\tInput day: ";
+        cin >> userDay;
+    } while (userDay < 1 || userDay > daysInMonth);
+    cout << "\nYour date of birth:\t" << userDay << "." << userMonth << "." << userYear << endl;
+    return 0;
+}
+

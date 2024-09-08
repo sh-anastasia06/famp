5a1f8cbf2ddf9cf99ec5b0a9f0261c614257aa43
Anastasia Shkundich <shkundich30@gmail.com>
2024-09-08 09:57:03 +0300

    lab 4

---
+++ lab 4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+// Вариант 4
+int main()
+{
+	int n = 6;
+	cout << "The multiplication table by " << n << endl;
+	for (int i = 1; i <= 10; i++) {
+		cout << i << " * " << n << " = " << i*n << endl;
+	}
+	return 0;
+}
+
---
+++ lab 4/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Input a: ";
+    cin >> a;
+    cout << "Input b: ";
+    cin >> b;
+    if (a > b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    for (int i = a; i <= b; i += 2) {
+        cout << i << "  ";
+    }
+    return 0;
+}

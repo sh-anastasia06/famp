9b8949eafbb6a7f21b6b58373750a781e9fca79e
Anastasia Shkundich <shkundich30@gmail.com>
2024-09-14 09:18:03 +0300

    lab 11

---
+++ lab 11/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int countSum(int num);
+void main()
+{
+    int number;
+    cout << "Input number > ";
+    cin >> number;
+    cout << "Summ = " << countSum(number) << endl;
+}
+int countSum(int num) {
+    if (num == 0) {
+        return num;
+    }
+    if (num > 0) {
+        return num + countSum(num - 1);
+    }
+    else {
+        return num + countSum(num + 1);
+    }
+    
+}
---
+++ lab 11/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int factorial(int n);
+void main()
+{
+    int number;
+    cout << "Input number > ";
+    cin >> number;
+    cout << factorial(number);
+}
+int factorial(int n) {
+    if (n == 1) {
+        cout << n << " = ";
+        return n;
+    }
+    cout << n << " * ";
+    return n * factorial(n - 1);
+}
